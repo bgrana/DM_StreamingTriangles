6bcc07879628507a6e7500bd376ae595b84548f1
Braulio <bgrana@conwet.com>
2016-12-03 12:31:24 +0100

    Initial commit

---
+++ src/StreamingTriangles.scala
@@ -0,0 +1,63 @@
+
+class StreamingTriangles(size_edges: Int, size_wedges: Int) {
+  
+  var edge_res = new Array[Tuple2[Int, Int]](size_edges)
+  var wedge_res = new Array[Tuple3[Int, Int, Int]](size_wedges)
+  var is_closed = new Array[Boolean](size_wedges)
+  var updated_edge_res = false
+  
+  private def closes(wedge: Tuple3[Int, Int, Int], edge: Tuple2[Int, Int]):Boolean = {
+    return (wedge._1 == edge._1 && wedge._3 == edge._2) || (wedge._1 == edge._2 && wedge._3 == edge._1)
+  }
+  
+  private def update_closed(new_edge: Tuple2[Int, Int]):Unit = {
+    for (i <- 0 to size_wedges) {
+      if (closes(wedge_res(i), new_edge)) {
+        is_closed(i) = true
+      }
+    }
+  }
+  
+  private def update_wedges_res(new_edge: Tuple2[Int, Int]):Unit = {
+    val x = scala.util.Random.nextFloat()
+    val tot_wedges = get_tot_wedges()
+    val new_wedges = get_new_wedges(new_edge)
+    val size_new_wedges = new_wedges.length
+    
+    for (i <- 0 to size_edges) {
+      if (x < 1/size_new_wedges/tot_wedges) {
+        val rand_index = scala.util.Random.nextInt(size_new_wedges)
+        wedge_res(i) = new_wedges(rand_index)
+        is_closed(i) = false
+      }
+    }
+  }
+  
+  private def update_edges_res(iter: Int, new_edge: Tuple2[Int, Int]):Unit = {
+    val x = scala.util.Random.nextFloat()
+    updated_edge_res = false
+    
+    for (i <- 0 to size_edges) {
+      if (x < 1/iter) {
+        edge_res(i) = new_edge
+        updated_edge_res = true
+      }
+    }
+  }
+  
+  private def get_tot_wedges():Int = {
+    return 12
+  }
+  
+  private def get_new_wedges(new_edge: Tuple2[Int, Int]):Array[Tuple3[Int, Int, Int]] = {
+    return wedge_res // TODO this is only a placeholder value
+  }
+  
+  def execute(new_edge: Tuple2[Int, Int], iter: Int) = {
+    update_closed(new_edge)
+    update_edges_res(iter, new_edge)
+    if (updated_edge_res) {
+      update_wedges_res(new_edge)
+    }
+  }
+}
