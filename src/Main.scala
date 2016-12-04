d1975d071ea0b594d6992fdf73c323e214130143
Braulio <bgrana@conwet.com>
2016-12-04 15:59:57 +0100

    Add Main

--- src/Main.scala
+++ src/Main.scala
@@ -2,4 +2,12 @@
 
 object Main {
   
+  def main(args: Array[String]): Unit = {
+    
+    val fpath = ""
+    val st = new StreamingTriangles(5,5)
+    val stream = st.getEdgeStream(fpath)
+    stream.foreach(st.execute)
+  }
+  
 }
